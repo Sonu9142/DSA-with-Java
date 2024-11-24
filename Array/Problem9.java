86c07e77f22e8d49d84993cee909df9ba0976154
Sonu Kumar Gupta <109494658+Sonu9142@users.noreply.github.com>
2024-11-24 17:03:29 +0000

    problem9

--- Array/Problem9.java
+++ Array/Problem9.java
@@ -1,3 +1,28 @@
 public class Problem9 {
-    
+    public static void PrintArray(int[] arr){
+        // int n= arr.length;
+        for(int num:arr){
+            System.err.println(num);
+        }
+    }
+    //check a string is pelindrome or not
+    public static boolean isPelindrome(String word){
+        char[] charArray= word.toCharArray();
+        int start=0;
+        int end=word.length()-1;
+        while(start<end){
+            if(charArray[start]!=charArray[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String sc="rotato";
+        System.out.println(isPelindrome(sc));
+        
+    }
 }
