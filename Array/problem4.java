5b4e4b047960966efa4b48f6a7836b2e9f46c237
Sonu Kumar Gupta <109494658+Sonu9142@users.noreply.github.com>
2024-11-24 16:00:57 +0000

    problem5

---
+++ Array/problem4.java
@@ -0,0 +1,31 @@
+public class problem4 {
+    public static void PrintArray(int[] arr){
+        // int n= arr.length;
+        for(int num:arr){
+            System.err.println(num);
+        }
+    }
+
+    // find 2nd maximum value in array
+
+    public static int SecMax(int[] arr){
+        int max= Integer.MIN_VALUE;
+        int secmax=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if (arr[i]>max) {
+                secmax=max;
+                max=arr[i];
+            }else if(arr[i]>secmax && arr[i] != max){
+                secmax=arr[i];
+            }
+        }
+        return secmax;
+    }
+
+    public static void main(String[] args) {
+        int[] myarr= {33,34,35,35};
+        PrintArray(myarr);
+
+        System.out.println(SecMax(myarr));
+    }
+}
