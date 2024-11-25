96ff00157528f32e94d28636026cd4306da3b44b
Sonu Kumar Gupta <109494658+Sonu9142@users.noreply.github.com>
2024-11-25 07:01:25 +0000

    count elements in singly linkedlist

--- Linked List/SinglyLinkedList.java
+++ Linked List/SinglyLinkedList.java
@@ -22,6 +22,19 @@ public class SinglyLinkedList {
         System.out.print("null");
     }
 
+    public int length(){
+        if(head==null){
+            return 0;
+        }
+        int count=0;
+        ListNode current=head;
+        while(current != null){
+            count++;
+            current=current.next;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll= new SinglyLinkedList();
         sll.head= new ListNode(10);
@@ -35,5 +48,6 @@ public class SinglyLinkedList {
         third.next=fourth; //10-->1-->8-->11-->null
 
         sll.display();
+        System.out.println(sll.length());
     }
 }
